9c82c45a957dec8b8e3d4d2d7094b2c638c8fdc2
osawa-koki <koki@osawa.dev>
2023-12-14 22:48:15 +0900

    ZIPスクリプトの作成。

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+import fs from 'fs'
+import JSZip from 'jszip'
+
+const zip = new JSZip()
+
+zip.file('hello.txt', 'Hello World\n')
+zip.file('hello2.txt', 'Hello World2\n')
+
+zip.generateAsync({ type: 'nodebuffer' })
+  .then((content) => {
+    fs.writeFileSync('example.zip', content)
+  }).catch((err) => {
+    console.error(err)
+  })
